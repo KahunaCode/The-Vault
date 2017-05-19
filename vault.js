b8176934c9358f892243b268d0dad2fd8396bb03
James Moore <jameslmoore@gmail.com>
2017-05-18 19:33:18 -1000

    1st test passes

--- vault.js
+++ vault.js
@@ -1,13 +1,27 @@
 'use strict';
 module.exports = function() {
 
+var key;
+var value;
+
+function setValue(key,value){
+  key=key;
+  value=value;
+  }
+function getValue(key){
+  if(key){
+    return value;
+  }
+  else{
+    return null;
+  }
+}
 
 
-
-
-
-
-
+return{
+  setValue:setValue,
+  getValue:getValue
+};
 
 
 };
